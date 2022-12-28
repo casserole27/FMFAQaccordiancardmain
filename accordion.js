4b2a1736b5567710a0358325a91f5a6fbba6c627
C Lewis <casserole.lewis@gmail.com>
2022-12-28 18:59:10 -0500

    Small updates, JS helpful links

--- accordion.js
+++ accordion.js
@@ -6,6 +6,7 @@ let acc = document.getElementsByClassName('accordion');
 //loop through accordion items
 
 for (let i = 0; i < acc.length; i++) {
+    console.log(acc[i]);
     acc[i].addEventListener("click", function() {
         /* Toggle between adding and removing the "active" class,*/
        
@@ -14,6 +15,8 @@ for (let i = 0; i < acc.length; i++) {
         /* Toggle between hiding and showing the active panel */
     let panel = this.nextElementSibling;
     
+        console.log(panel);
+
     if (panel.style.display === "block") {
           panel.style.display = "none";
     } else {
@@ -21,3 +24,13 @@ for (let i = 0; i < acc.length; i++) {
     }
       });
     } 
+
+
+    //! HELPFUL LINKS
+    //! getElementsByClassName: https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
+    //! this keyword: https://www.w3schools.com/js/js_this.asp
+    //! classList: https://www.w3schools.com/jsref/prop_element_classlist.asp
+    //! for loops: https://www.w3schools.com/js/js_loop_for.asp
+    //! nextElementSibling: https://www.w3schools.com/js/js_loop_for.asp
+    //! HTML DOM element objects, properties and methods: https://www.w3schools.com/jsref/dom_obj_all.asp
+    
