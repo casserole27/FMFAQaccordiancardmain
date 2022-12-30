cedabe2851d5c23ef4d95e151bcbf1649176382c
C Lewis <casserole.lewis@gmail.com>
2022-12-30 11:21:21 -0500

    Code cleanup

--- accordion.js
+++ accordion.js
@@ -1,18 +1,22 @@
 
-//define accordian variable 
+//TODO
+//TODO W3 Schools accordion menu tutorial: https://www.w3schools.com/howto/howto_js_accordion.asp
+
+
+//TODO define accordian variable 
 
 let acc = document.getElementsByClassName('accordion');
 
-//loop through accordion items
+//TODO loop through accordion items
 
 for (let i = 0; i < acc.length; i++) {
     console.log(acc[i]);
     acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,*/
+        //TODO Toggle between adding and removing the "active" class
        
     this.classList.toggle("active");
     
-        /* Toggle between hiding and showing the active panel */
+        //TODO Toggle between hiding and showing the active panel 
     let panel = this.nextElementSibling;
     
         console.log(panel);
@@ -26,11 +30,11 @@ for (let i = 0; i < acc.length; i++) {
     } 
 
 
-    //! HELPFUL LINKS
-    //! getElementsByClassName: https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
-    //! this keyword: https://www.w3schools.com/js/js_this.asp
-    //! classList: https://www.w3schools.com/jsref/prop_element_classlist.asp
-    //! for loops: https://www.w3schools.com/js/js_loop_for.asp
-    //! nextElementSibling: https://www.w3schools.com/js/js_loop_for.asp
-    //! HTML DOM element objects, properties and methods: https://www.w3schools.com/jsref/dom_obj_all.asp
+    //? HELPFUL LINKS
+    //? getElementsByClassName: https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
+    //? this keyword: https://www.w3schools.com/js/js_this.asp
+    //? classList: https://www.w3schools.com/jsref/prop_element_classlist.asp
+    //? for loops: https://www.w3schools.com/js/js_loop_for.asp
+    //? nextElementSibling: https://www.w3schools.com/js/js_loop_for.asp
+    //? HTML DOM element objects, properties and methods: https://www.w3schools.com/jsref/dom_obj_all.asp
     
